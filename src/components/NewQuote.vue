0d06cb3e2e2402ae475773521d1b01f8183e675f
Oren E <oepsteindev@gmail.com>
2018-08-13 15:03:06 -0400

    flies

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="row">
+	<form>
+		<div class="col-sm-8 col-sm-offset-2 form-group">
+			<label>Quote</label>
+			<textarea class="form-control" rows="3" v-model="quote"></textarea>
+			<button class="btn btn-primary"  @click.prevent="createNew">Add Quote</button>
+		</div>
+	</form>
+</div>
+</template>
+<script>
+	
+export default {
+	data: function() {
+		return {
+			quote: ''
+		};
+	},
+	methods: {
+		createNew() {
+			this.$emit('quoteAdded', this.quote);
+			this.quote = '';
+		}
+	}
+}
+	
+</script>
+<style>
+</style>
